c11cb0b06ebf6a4b6007ccc3d23d515db9b36655
nae-stormrage <stas.markus.2004@mail.ru>
2024-12-13 21:47:58 +0300

    Добавьте файлы проекта.

---
+++ Lab_12/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab_12
+{
+    public partial class Form1 : Form
+    {
+        string arg01;
+        string arg02;
+        string arg03;
+        string arg04;
+        string arg05;
+        double RES;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = (e.X).ToString();
+            textBox2.Text = (e.Y).ToString();
+            arg01 = textBox1.Text;
+            arg02 = textBox2.Text;
+            arg03 = textBox3.Text;
+            arg04 = textBox4.Text;
+            arg05 = textBox5.Text;
+            try
+            {
+                double c_arg01 = Convert.ToDouble(arg01);
+                double c_arg02 = Convert.ToDouble(arg02);
+                double c_arg03 = Convert.ToDouble(arg03);
+                double c_arg04 = Convert.ToDouble(arg04);
+                double c_arg05 = Convert.ToDouble(arg05);
+                RES = (c_arg01 - c_arg02 + Math.Abs(Math.Sin(c_arg03) + Math.Sqrt(Math.Abs(c_arg04))))/(Math.Pow(c_arg01, c_arg05) - Math.Log10(c_arg02));
+                Text = ("RES = " + RES.ToString());
+            } catch
+            {
+                Text = string.Format("ERROR");
+            }
+        }
+    }
+}
---
+++ Lab_12/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Lab_12")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab_12")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d63745a9-f2c4-4a5d-9dcd-f98da97b4097")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
